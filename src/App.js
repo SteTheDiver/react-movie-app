fc58365836a6bd69ecff34864a8c57c29b2744b8
SteTheDiver <ste@name.com>
2020-12-01 14:37:29 +0100

    making the SearchBar component to work

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  // fetching data from API
-  const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=48050f69`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -19,11 +21,15 @@ const App = () => {
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
--- src/components/MovieListHeading.jsx
+++ src/components/MovieListHeading.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MovieListHeading = () => {
+const MovieListHeading = (props) => {
   return (
     <div className="col">
       <h1>{props.heading}</h1>
